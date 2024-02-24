0008a51dda9c35335aee2d7231543795a287006a
saurabh.yadav <saurabh.yadav@deeptek.ai>
2024-02-24 22:15:27 +0530

    Footer Added

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from home import views
+from django.contrib import admin
 
+admin.site.site_header = "Travel India"
+admin.site.site_title = "वसुधैव कुटुम्बकम्"
+admin.site.index_title = "Love India"
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
